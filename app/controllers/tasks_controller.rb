76436208a0b9f76c0722c7cef89c1a72f42ac042
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-09-27 17:00:32 -0700

    Working index. In progress: show getting material from index

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+  attr_accessor :tasks
+
+  def index
+    @tasks = [
+      {
+        name: "read",
+        description: "read books",
+        completion_status: "in progress",
+        completion_date: "TBD"
+      },
+      {
+        name: "cook",
+        description: "chop and heat food in a delicious manner",
+        completion_status: "not started",
+        completion_date: "today, 6pm"
+      },
+      {
+        name: "walk",
+        description: "take imaginary dog for a walk",
+        completion_status: "done",
+        completion_date: "today, 7am"
+      }
+    ]
+  end
+
+  def show
+    @id = params[:id]
+
+    # @new_task = {
+    #   name: params[:name],
+    #   description: params[:description],
+    #   completion_status: params[:completion_status],
+    #   completion_date: params[:completion_date],
+    # }
+
+    # @new_task = @tasks[0]
+
+  end
+end
