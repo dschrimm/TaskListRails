d21a759a74079cebd8d4447cdae89c680babe7c8
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-07 09:33:01 -0700

    Update gemfile to add pg as the production gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
+
+  # resources :tasks, except: [:create, :new, :edit, :update]
   root 'tasks#index'
   get '/tasks' => 'tasks#index'
-
+  #
   get '/tasks/new' => 'tasks#new', as: 'task_new'
   post '/tasks' => 'tasks#create'
-
+  #
   get '/tasks/:id', to: 'tasks#show', as: 'task'
-
+  #
   get '/tasks/:id/edit' => 'tasks#edit', as: 'task_edit'
   patch '/tasks/:id' => 'tasks#update'
-
+  #
   get '/tasks/:id/completed', to: 'tasks#complete', as: 'task_complete'
   patch '/tasks/:id' => 'tasks#update'
-
+  #
   delete '/tasks/:id' => 'tasks#destroy'
 
 end
