e4e1f48075d58bb2500be553c889fba68220166d
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-19 21:25:42 -0700

    Add some user test ideas

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,13 @@ class UserTest < ActiveSupport::TestCase
   test "Create a valid user" do
     assert users(:snoopy).valid?
   end
+
+  # test "If a user is not logged in they cannot see their task." do
+  #   session[:user_id] = nil  # ensure no one is logged in
+  #
+  #   get :show, id: tasks(:sample_task).id
+  #   # if they are not logged in they cannot see the resource and are redirected to login.
+  #   assert_redirected session_path
+  #   assert_equal "You must be logged in first", flash[:notice]
+  # end
 end
